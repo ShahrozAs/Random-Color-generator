7de01d3ab2e5df8eff0fae3ba993544d78067fc4
Mr Sherry <mr.sherry.793@gmail.com>
2023-07-20 12:51:19 +0500

    adding button color

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-let randomNumber;
-const codeGenerator=()=>{
+ let randomNumber;
+ const codeGenerator=()=>{
+     let prev=document.getElementById('code-number').innerHTML;
+     
+     const random=Math.floor(Math.random()*16777215);
+     randomNumber="#"+random.toString(16);
+     document.body.style.backgroundColor=randomNumber;
+     document.getElementById('code-number').innerHTML=randomNumber;
+     
+   
 
-    const random=Math.floor(Math.random()*16777215);
-    randomNumber="#"+random.toString(16);
-    document.body.style.backgroundColor=randomNumber;
-    document.getElementById('code-number').innerText=randomNumber;
-}
-
+    document.getElementById('btn2').style.backgroundColor=prev;
+    document.getElementById('btn1').style.backgroundColor=randomNumber;
+     
+    }
+    
 const copyText=()=>{
     navigator.clipboard.writeText(randomNumber);
     document.getElementById('btn1').innerHTML=`Copied!`
+    
+    
+    setTimeout(() => {
+        
+        document.getElementById('btn1').innerHTML=`  Copy text`
+    }, 1000);
 
 }
 
